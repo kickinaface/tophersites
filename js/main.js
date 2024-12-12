697d34d64bc7f26ab7df723efdaf7f3b9ebeefa7
Chris Carter <obsidian@Helios.local>
2024-12-12 09:32:00 -0500

    First commit add files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var linkList = [
+    {
+        title:"Alphabet Scriptor (Web Version)",
+        url:"https://bs37v5e6er.loclx.io"
+    },
+    {
+        title:"HP Machine 1 (Waterblaze) Dashboard",
+        url:"https://kjdgzhebdg.loclx.io"
+    },
+    {
+        title:"Dell XPS 400 (Linux)",
+        url:"https://6ymhookxez.loclx.io"
+    }
+]
+
+document.addEventListener("DOMContentLoaded", function(e){
+    //dom content loaded, run script.
+    var siteLinks = document.querySelector(".siteLinks");
+    // for(var i = 0; i<= linkList.length-1; i++){
+    //     siteLinks.innerHTML+="<a href='https://bs37v5e6er.loclx.io'>Alphabet Scriptor (Web Version)</a> <button onclick=location.href='mailto:6142089419@txt.att.net?subject=`Broken Link: Alphabet Scriptor'>Report Broken</button><br>";
+    // }
+});
