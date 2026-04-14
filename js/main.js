44b2a487728e62d0b06d6b6ee721318a07c63a42
Christopher Carter <kickinaface@gmail.com>
2026-04-14 08:58:01 -0400

    update js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,14 @@
-var linkList = [
-    {
-        title:"Alphabet Scriptor (Web Version)",
-        url:"https://bs37v5e6er.loclx.io"
-    },
-    {
-        title:"HP Machine 1 (Waterblaze) Dashboard",
-        url:"https://kjdgzhebdg.loclx.io"
-    },
-    {
-        title:"Dell XPS 400 (Linux)",
-        url:"https://6ymhookxez.loclx.io"
-    }
-]
-
 document.addEventListener("DOMContentLoaded", function(e){
-    //dom content loaded, run script.
-    //var siteLinks = document.querySelector(".siteLinks");
-    // for(var i = 0; i<= linkList.length-1; i++){
-    //     siteLinks.innerHTML+="<a href='https://bs37v5e6er.loclx.io'>Alphabet Scriptor (Web Version)</a> <button onclick=location.href='mailto:6142089419@txt.att.net?subject=`Broken Link: Alphabet Scriptor'>Report Broken</button><br>";
-    // }
-
     fetch('./js/messages.json')
         .then(function(data){
-            if(data.showMessages == true){
+            console.log("data ", data[0]);
+            if(data[0].showMessages == true){
                 document.querySelector(".dashboardMessages").style.display = "block";
                 document.querySelector(".dashboardMessages span").innerHTML = data.message;
             }
         })
         .then(function(data){
-            console.log(data);
+            //console.log(data);
         })
         .catch(function(err){
             console.error('Error:', err)
