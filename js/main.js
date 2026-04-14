5d40d54defdbfbe2d408a5c4804be7fd191d98d1
Christopher Carter <kickinaface@gmail.com>
2026-04-14 08:44:10 -0400

    update: add messages

--- js/main.js
+++ js/main.js
@@ -15,8 +15,22 @@ var linkList = [
 
 document.addEventListener("DOMContentLoaded", function(e){
     //dom content loaded, run script.
-    var siteLinks = document.querySelector(".siteLinks");
+    //var siteLinks = document.querySelector(".siteLinks");
     // for(var i = 0; i<= linkList.length-1; i++){
     //     siteLinks.innerHTML+="<a href='https://bs37v5e6er.loclx.io'>Alphabet Scriptor (Web Version)</a> <button onclick=location.href='mailto:6142089419@txt.att.net?subject=`Broken Link: Alphabet Scriptor'>Report Broken</button><br>";
     // }
+
+    fetch('https://ryzen.flounder-lime.ts.net/uploads/Development/dashboardMessages/messages.json')
+        .then(function(data){
+            if(data.showMessages == true){
+                document.querySelector(".dashboardMessages").style.display = "block";
+                document.querySelector(".dashboardMessages span").innerHTML = data.message;
+            }
+        })
+        .then(function(data){
+            console.log(data);
+        })
+        .catch(function(err){
+            console.error('Error:', err)
+        });
 });
