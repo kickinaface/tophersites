a0c95053e897b02c16fdfdbffc87789ed5b391aa
Christopher Carter <kickinaface@gmail.com>
2026-04-14 09:10:27 -0400

    update message wrapper

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     //load messages
     var dashboardMessages = document.querySelector(".dashboardMessages");
     if(homepageMessages.showMessages == true){
+        dashboardMessages.style.display = "block";
         dashboardMessages.querySelector("span").innerHTML = homepageMessages.message;
     }
 });
