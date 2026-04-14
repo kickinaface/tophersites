da8aa57e775646f6965c0819138ca1f2494943ec
Christopher Carter <kickinaface@gmail.com>
2026-04-14 09:02:34 -0400

    mod js Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", function(e){
     fetch('./js/messages.json')
         .then(function(data){
             console.log("data ", data);
+            console.log(JSON.stringify(data));
+            console.log(JSON.parse(data));
             if(data[0].showMessages == true){
                 document.querySelector(".dashboardMessages").style.display = "block";
                 document.querySelector(".dashboardMessages span").innerHTML = data.message;
             }
         })
         .then(function(data){
-            //console.log(data);
+            console.log(data);
         })
         .catch(function(err){
             console.error('Error:', err)
